4a915acf188f8ec055955287ddff5caa43b977db
renjiayuan <47494304+renjiayuan@users.noreply.github.com>
2022-03-27 11:43:36 +0800

    Add files via upload

---
+++ Employee management system/boss.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+#include "worker.h"
+using namespace std;
+
+class Boss:public Worker{
+public:
+    //构造函数
+    Boss(int id,string name,int dId);
+    //显示个人信息
+    virtual void showInfo();
+    //获取岗位名称
+    virtual string getDeptName();
+};
